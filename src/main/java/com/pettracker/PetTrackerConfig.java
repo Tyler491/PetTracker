31b9fbc38da9e7cbdadc7f2b7a1a16e8f2796617
Tyler <tdbuch6@yahoo.com>
2024-01-26 17:41:14 -0700

    Name change from Example to Pet Tracker.

--- src/main/java/com/pettracker/PetTrackerConfig.java
+++ src/main/java/com/pettracker/PetTrackerConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.pettracker;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface PetTrackerConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/com/pettracker/PetTracker.java
+++ src/test/java/com/pettracker/PetTracker.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.pettracker;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PetTracker
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PetTrackerPlugin.class);
 		RuneLite.main(args);
 	}
 }
